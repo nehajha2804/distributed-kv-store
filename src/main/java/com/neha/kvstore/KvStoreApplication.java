232ca536fd0eae0ae822a3a60ecc517c5dc481ed
Neha <nehajha2804@gmail.com>
2026-08-01 17:03:59 +0530

    Updated version to 3.3.13 to fixed known CVEs

---
+++ src/main/java/com/neha/kvstore/KvStoreApplication.java
@@ -0,0 +1,10 @@
+package com.neha.kvstore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvStoreApplication {
+    public static void main(String args[]){
+        SpringApplication.run(KvStoreApplication.class, args);
+    }
+}
