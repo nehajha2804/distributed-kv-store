fe5bffb1eb65599d8729f0b253e7fac6488c18b5
Neha <nehajha2804@gmail.com>
2026-08-03 17:00:51 +0530

    Modified KeeyValueStore

---
+++ src/main/java/com/neha/kvstore/network/StoreController.java
@@ -0,0 +1,2 @@
+package com.neha.kvstore.network;public class StoreController {
+}
--- src/main/java/com/neha/kvstore/node/KeyValueStore.java
+++ src/main/java/com/neha/kvstore/node/KeyValueStore.java
@@ -1,4 +1,27 @@
 package com.neha.kvstore.node;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
 public class KeyValueStore {
+    private final Map<String, String> store = new ConcurrentHashMap<>();
+
+    public void put(String key, String value) {
+        store.put(key, value);
+    }
+
+    public String get(String key){
+        return store.get(key);
+    }
+
+    public boolean delete(String key){
+        return store.remove(key) != null; //returns the previous value associated with the key
+    }
+
+    public int size(){
+        return store.size();
+    }
 }
