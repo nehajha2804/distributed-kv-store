d43da2f790ad5f78a9332f91a8a9b84d4ba6eb04
Neha <nehajha2804@gmail.com>
2026-08-03 18:38:16 +0530

    Added Application.properties to resources

--- src/main/java/com/neha/kvstore/network/StoreController.java
+++ src/main/java/com/neha/kvstore/network/StoreController.java
@@ -1,4 +1,46 @@
 package com.neha.kvstore.network;
 
+import com.neha.kvstore.node.KeyValueStore;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/store")
 public class StoreController {
+    private final KeyValueStore store;
+
+    public StoreController(KeyValueStore store){
+        this.store = store;
+    }
+
+    @PutMapping("/{key}")
+    public ResponseEntity<String> put(@PathVariable String key, @RequestBody String value){
+        store.put(key, value);
+        return ResponseEntity.ok("Stored:" + key + " = " + value);
+    }
+
+    @GetMapping("/{key}")
+    public ResponseEntity<String> get(@PathVariable String key){
+        String value =  store.get(key);
+        if(value == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(value);
+    }
+
+    @DeleteMapping("/{key}")
+    public ResponseEntity<String> delete(@PathVariable String key) {
+        boolean deleted = store.delete(key);
+        if (!deleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok("Deleted: " + key);
+    }
+
+    @GetMapping("/_size")
+    public ResponseEntity<Integer> size() {
+        return ResponseEntity.ok(store.size());
+    }
+
 }
